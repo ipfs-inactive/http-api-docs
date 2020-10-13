46088b69267618f05ea42822630ccca847367443
Oli Evans <oli@tableflip.io>
2020-10-13 13:15:41 +0100

    fix: ignore options per endpoint
    This PR now requires options that should be ignored be listed per endpoint, to avoid removing docs from other endpoints unknowingly.
    
    The global "ignore these options" logic was causing us to skip generating docs for the recursive flag on several endpoints
    
    - `ipfs dht provide -r` - ["Recursively provide entire graph."](https://github.com/ipfs/go-ipfs/blob/e1e577fe4e9cd2cda44a158c4652437311919d2a/core/commands/dht.go#L232)
    - `ipfs dns -r` -  ["Resolve until the result is not a DNS link."](https://github.com/ipfs/go-ipfs/blob/e1e577fe4e9cd2cda44a158c4652437311919d2a/core/commands/dns.go#L60)
    - `ipfs pin add -r` - ["Recursively pin the object linked to by the specified object(s)."](https://github.com/ipfs/go-ipfs/blob/e1e577fe4e9cd2cda44a158c4652437311919d2a/core/commands/pin.go#L65)
    - `ipfs refs -r` - ["Recursively list links of child nodes."](https://github.com/ipfs/go-ipfs/blob/e1e577fe4e9cd2cda44a158c4652437311919d2a/core/commands/refs.go#L68)
    - `ipfs resolve -r` - ["Resolve until the result is an IPFS name."](https://github.com/ipfs/go-ipfs/blob/e1e577fe4e9cd2cda44a158c4652437311919d2a/core/commands/resolve.go#L72)
    - `ipfs name resolve -r` ["Resolve until the result is not an IPNS name."](https://github.com/ipfs/go-ipfs/blob/e1e577fe4e9cd2cda44a158c4652437311919d2a/core/commands/name/ipns.go#L76)
    
    All of the options listed in the clientOpts map applied only to `ipfs add`.
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- endpoints.go
+++ endpoints.go
@@ -23,13 +23,15 @@ var JsondocGlossary = jsondoc.NewGlossary().
 	WithSchema(new(peerstore.PeerInfo),
 		jsondoc.Object{"ID": "peer-id", "Addrs": []string{"<multiaddr-string>"}})
 
-var clientOpts = map[string]struct{}{
-	cmds.RecLong:     struct{}{},
-	cmds.DerefLong:   struct{}{},
-	cmds.StdinName:   struct{}{},
-	cmds.Hidden:      struct{}{},
-	cmds.Ignore:      struct{}{},
-	cmds.IgnoreRules: struct{}{},
+var ignoreOptsPerEndpoint = map[string]map[string]struct{}{
+	"/api/v0/add": {
+		cmds.RecLong:     struct{}{},
+		cmds.DerefLong:   struct{}{},
+		cmds.StdinName:   struct{}{},
+		cmds.Hidden:      struct{}{},
+		cmds.Ignore:      struct{}{},
+		cmds.IgnoreRules: struct{}{},
+	},
 }
 
 // A map of single endpoints to be skipped (subcommands are processed though).
@@ -103,11 +105,9 @@ func Endpoints(name string, cmd *cmds.Command) (endpoints []*Endpoint) {
 		}
 
 		for _, opt := range cmd.Options {
-			if _, ok := clientOpts[opt.Names()[0]]; ok {
-				if opt.Names()[0] == cmds.RecLong && name == "/api/v0/refs" {
-					// special case: do include the `recursive` option for the refs endpoint
-				} else {
-					// otherwise its a client-side option; skip it.
+			if ignoreOpts, ok := ignoreOptsPerEndpoint[name]; ok {
+				if _, ok := ignoreOpts[opt.Names()[0]]; ok {
+					// skip this option for this endpoint.
 					continue
 				}
 			}
