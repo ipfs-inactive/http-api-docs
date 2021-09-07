b7a02c3d11346a59957d4fefd08a821b75ebb8bf
Petar Maymounkov <petarm@gmail.com>
2021-09-07 15:15:04 -0700

    add beginnings of update action

--- .github/actions/check-for-new-ipfs-tag/entrypoint.sh
+++ .github/actions/check-for-new-ipfs-tag/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
-# XXX: extract go-ipfs release from go.mod in this repo
+# extract go-ipfs release tag used in http-api-docs from go.mod in this repo
+CURRENT_IPFS_TAG = $(grep 'github.com/ipfs/go-ipfs ' ./http-api-docs/go.mod | awk '{print $2}')
+
+echo "The currently used IPFS tag is ${CURRENT_IPFS_TAG}"
 
 # XXX: extract release XXX
