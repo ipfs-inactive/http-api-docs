ff051bb06caf144b4bc29e23d6372aaec3dda8b4
Petar Maymounkov <petarm@gmail.com>
2021-09-08 07:45:53 -0700

    ok

--- .github/actions/latest-ipfs-tag/entrypoint.sh
+++ .github/actions/latest-ipfs-tag/entrypoint.sh
@@ -7,4 +7,4 @@ git clone https://github.com/ipfs/go-ipfs.git
 cd go-ipfs
 LATEST_IPFS_TAG=`git describe --tags --abbrev=0`
 echo "The latest IPFS tag is ${LATEST_IPFS_TAG}"
-echo "::set-output name=latest-tag::${LATEST_IPFS_TAG}"
+echo "::set-output name=latest_tag::${LATEST_IPFS_TAG}"
